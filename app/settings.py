2e9104f467a1108d57174834fbafa733f65d6dff
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-12-07 22:34:30 +0200

    settings & requirements.txt

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-!msr_g6+dwnudtl5fjzsz2rqcwq+8c)_!b*mqh%kq%)7o671r@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
